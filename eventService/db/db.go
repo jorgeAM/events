4a59356af817d56968f19f7c44e2cd9151e34a29
Jorge L <jorge.alfmur@gmail.com>
2020-05-11 03:59:40 -0500

    initialize persistence layer

---
+++ eventService/db/db.go
@@ -0,0 +1,21 @@
+package db
+
+import "github.com/jorgeAM/events/eventService/db/mongo"
+
+// TYPE is a custom type to handle database
+type TYPE string
+
+// constants
+const (
+	MONGODB = "mongodb"
+)
+
+// NewPersistenceLayer initialize persistenceLayer
+func NewPersistenceLayer(database TYPE, url string) (DatabaseHandler, error) {
+	switch database {
+	case MONGODB:
+		return mongo.NewMongoDBLayer(url)
+	default:
+		return nil, nil
+	}
+}
--- eventService/db/handler.go
+++ eventService/db/handler.go
@@ -3,7 +3,7 @@ package db
 import "github.com/jorgeAM/events/eventService/models"
 
 // DatabaseHandler handles method in database
-type databaseHandler interface {
+type DatabaseHandler interface {
 	CreateEvent(event *models.Event) ([]byte, error)
 	ListAvailableEvents() ([]models.Event, error)
 	GetEvent(id string) (*models.Event, error)
