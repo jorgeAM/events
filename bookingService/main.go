f9577e1695ba8803b3d994ae5c8d3f7edd8314b5
Jorge L <jorge.alfmur@gmail.com>
2020-05-12 19:08:53 -0500

    connect to rabbitmq - bookingService

--- bookingService/main.go
+++ bookingService/main.go
@@ -6,10 +6,26 @@ import (
 
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/jorgeAM/events/bookingService/server"
+	"github.com/jorgeAM/events/msgbroker/rabbitmq"
+	"github.com/streadway/amqp"
 )
 
 func main() {
 	endpoint := os.Getenv("ENDPOINT")
+	amqpURL := os.Getenv("AMQP_URL")
+
+	log.Println("connecting to message broker")
+	conn, err := amqp.Dial(amqpURL)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = rabbitmq.NewAmqpEventListener(conn, "events")
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	log.Println("Server starts running")
 	log.Fatal(server.Listen(endpoint))
